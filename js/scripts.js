0b9604f940a0dfded1cb5e615b0c17f781dd8cde
Ashley Marie Laing <ashleym.laing@gmail.com>
2016-12-02 12:55:45 -0500

    used angular js to repeat the catergories and added the img to each one as well as links to the atricle opening up to a different tab.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,63 @@
 /**
  * Created by Unaka Muigai on 9/13/2016.
  */
+var app = angular.module('portfolio', []);
+
+app.controller('featuredCtrl', function($scope) {
+$scope.articles = [
+  {
+    category: "Public Policy",
+    logo:"fortune-logo.jpg",
+    title:"Brewing a better beer for Africa",
+    date: "June 24, 2011",
+    description:"Fortune -- A guy walks into a bar and orders a pint of beer. But this isn't a pub in London or a sports bar in Milwaukee -- it's a watering hole in Uganda.",
+    link:"http://fortune.com/2011/06/24/brewing-a-better-beer-for-africa/",
+    hasPDF:false
+  },
+  {
+    category:"Business",
+    logo:"fortune-logo.jpg",
+    title:"Brewing a better beer for Africa",
+    date: "June 24, 2011",
+    description:"Fortune -- A guy walks into a bar and orders a pint of beer. But this isn't a pub in London or a sports bar in Milwaukee -- it's a watering hole in Uganda.",
+    link:"http://fortune.com/2011/06/24/brewing-a-better-beer-for-africa/",
+    hasPDF:false
+  },
+  {
+    category:"News",
+    logo:"fortune-logo.jpg",
+    title:"Brewing a better beer for Africa",
+    date: "June 24, 2011",
+    description:"Fortune -- A guy walks into a bar and orders a pint of beer. But this isn't a pub in London or a sports bar in Milwaukee -- it's a watering hole in Uganda.",
+    link:"http://fortune.com/2011/06/24/brewing-a-better-beer-for-africa/",
+    hasPDF:false
+  },
+  {
+    category:"Science",
+    logo:"fortune-logo.jpg",
+    title:"Brewing a better beer for Africa",
+    date: "June 24, 2011",
+    description:"Fortune -- A guy walks into a bar and orders a pint of beer. But this isn't a pub in London or a sports bar in Milwaukee -- it's a watering hole in Uganda.",
+    link:"http://fortune.com/2011/06/24/brewing-a-better-beer-for-africa/",
+    hasPDF:false
+  },
+  {
+    category:"Travel",
+    logo:"fortune-logo.jpg",
+    title:"Brewing a better beer for Africa",
+    date: "June 24, 2011",
+    description:"Fortune -- A guy walks into a bar and orders a pint of beer. But this isn't a pub in London or a sports bar in Milwaukee -- it's a watering hole in Uganda.",
+    link:"http://fortune.com/2011/06/24/brewing-a-better-beer-for-africa/",
+    hasPDF:false
+  },
+  {
+    category:"Other",
+    logo:"fortune-logo.jpg",
+    title:"Brewing a better beer for Africa",
+    date: "June 24, 2011",
+    description:"Fortune -- A guy walks into a bar and orders a pint of beer. But this isn't a pub in London or a sports bar in Milwaukee -- it's a watering hole in Uganda.",
+    link:"http://fortune.com/2011/06/24/brewing-a-better-beer-for-africa/",
+    hasPDF:false
+  }
+];
+});
